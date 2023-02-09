24da7a67f6ebe8abd851dd8bd29ca0b0c3e3c864
ihsan <ihsan.alaqib@bjitacademy.com>
2023-02-09 16:57:48 +0600

    adapter attached but no working

---
+++ app/src/main/java/com/ihsan/cricplanet/adapter/TeamAdapter.kt
@@ -0,0 +1,79 @@
+package com.ihsan.cricplanet.adapter
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.ihsan.cricplanet.R
+import com.ihsan.cricplanet.model.Team
+import com.ihsan.cricplanet.viewmodel.CricViewModel
+import com.squareup.picasso.Picasso
+
+class TeamAdapter(
+    private val teamList: ArrayList<Team>
+) : RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() {
+    //private val viewModel: CricViewModel = CricViewModel(application = Application())
+
+    class TeamViewHolder(private val binding: View) : RecyclerView.ViewHolder(binding){
+        val teamName: TextView =binding.findViewById(R.id.team)
+        val image: ImageView =itemView.findViewById(R.id.image)
+        val description: TextView =itemView.findViewById(R.id.description)
+        val source: TextView =itemView.findViewById(R.id.source)
+        val btnFavourite: ImageButton =itemView.findViewById(R.id.favourite)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
+        val root= LayoutInflater.from(parent.context).inflate(R.layout.team_item,parent,false)
+        Log.d("teamAdapter", "onCreateViewHolder: ${teamList.size}")
+        return TeamViewHolder(root)
+    }
+
+    override fun getItemCount(): Int {
+        return teamList.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+        val team=teamList[position]
+        Log.d("teamAdapter", "BindViewHolder: ${teamList.size}")
+        holder.teamName.text=team.name
+        /*holder.description.text="Id: ${team.id}${team.resource} updated at:${team.updated_at}"
+
+        holder.source.text=team.national_team.toString()
+
+        if(!TextUtils.isEmpty(team.image_path))
+        {
+            Picasso.get().load(team.image_path).fit().centerCrop().placeholder(R.drawable.progress_animation).into(holder.image)
+        }
+        else{
+            holder.image.setImageResource(R.drawable.ic_image)
+        }
+        holder.itemView.setOnClickListener{
+            //showStyledSnackbar(it, team.name)
+            //notifyDataSetChanged()
+        }*/
+    }
+
+    fun showStyledSnackbar(view: View, text: String) {
+        val snackbar = Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(Color.parseColor("#3F51B5"))
+        val textView = snackbarView.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+        textView.setTextColor(Color.WHITE)
+        textView.textSize = 20f
+        textView.setTypeface(null, Typeface.BOLD)
+        snackbar.show()
+    }
+}
--- app/src/main/java/com/ihsan/cricplanet/ui/fragment/TeamFragment.kt
+++ app/src/main/java/com/ihsan/cricplanet/ui/fragment/TeamFragment.kt
@@ -7,13 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ihsan.cricplanet.R
+import com.ihsan.cricplanet.adapter.TeamAdapter
 import com.ihsan.cricplanet.databinding.FragmentTeamBinding
+import com.ihsan.cricplanet.model.Team
+import com.ihsan.cricplanet.utils.MyApplication
 import com.ihsan.cricplanet.viewmodel.CricViewModel
 
 class TeamFragment : Fragment() {
     private lateinit var binding: FragmentTeamBinding
     private val viewModel: CricViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +33,12 @@ class TeamFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getTeams.observe(requireActivity()) {
+        viewModel.getTeams.observe(viewLifecycleOwner) {
             Log.d("cricTeam", "onViewCreated Team teamList: ${it.size}")
+            recyclerView = binding.recyclerviewTeam
+            recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+            recyclerView.adapter = TeamAdapter(it as ArrayList<Team>)
+            recyclerView.setHasFixedSize(true)
             if (it.isEmpty()) {
                 Log.d("newsHome", "onViewCreated with empty roomData: APi Call ")
                 viewModel.getTeams()
